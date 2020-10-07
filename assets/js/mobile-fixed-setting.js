adfa44c164bbbbb81f2f0d939f7617cc1670f6c1
IamCooller <45273146+IamCooller@users.noreply.github.com>
2020-10-07 19:54:26 +0300

    Add files via upload

--- assets/js/mobile-fixed-setting.js
+++ assets/js/mobile-fixed-setting.js
@@ -1,3 +1,20 @@
 $('.navbar-toggler').on('click', function () {
     $('.navbar').toggleClass('positionFixed');    
 });
+
+
+    $(function() {
+    $(window).scroll(function() {
+    var scroll = $(window).scrollTop() + 90;
+    var currentArea = $("section").filter(function() {
+    return scroll <= $(this).offset().top + $(this).height();
+    });
+    if ($(window).scrollTop() > 100) {
+    $('.navbar').addClass("navScroll");
+
+    } else if ($(window).scrollTop() < 100 ) {
+    $('.navbar').removeClass("navScroll");
+
+    }
+    });
+    });
